3d4b0a360dbeb09da84a22ac28857153445d7ad0
linda <lindatochukwu30@gmail.com>
2020-11-10 08:50:24 +0100

    first commit

---
+++ src/Components/Body.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Body = () => {
+    return (
+
+        <>
+            <div className="row">
+                <div className="col-6">
+                    this is the body
+        </div>
+
+            </div>
+        </>
+
+    )
+}
+
+
+export default Body
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+
+        <>
+        <h1>
+            kkkkkk
+        </h1>
+            <div className="row bg-black text-white">
+                <div className="col-12">
+                    i am a boy
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+
+export default Footer
