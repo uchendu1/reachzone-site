2ccb6f210057b0a28fd946709f6bbe58022978eb
linda <lindatochukwu30@gmail.com>
2020-11-17 12:22:34 +0100

    remove images and replaced with divs

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -33,9 +33,104 @@ const Body = () => {
                 </div>
 
 
+                <div className="row justify-content-center row1" >
+                    <div className="col-11 black1 ">
+                        <div className="col blackkk" >
+                            <button className="btn btn-light btn1 ">
+                                COMING SOON
+                            </button>
+                        </div>
+                        <div>
+                        <p className="remote mt-3">
+                            RemoteAfrica
+                        </p>
+                        <p className="branding">
+                            Branding, Web design
+                        </p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="row parent-row ">
+                    <div className="col-md-4 black1 left">
+                        <div className="col-md-12 blackk" >
+                            <button className="btn btn-light btn1 ">
+                                COMING SOON
+                            </button>
+                        </div>
+                        <div>
+                            <p className="remote mt-3">
+                                Wanted
+                                </p>
+                            <p className="branding">
+                                Web design
+                        </p>
+                        </div>
+
+                    </div>
+
+                    <div className="col-md-4 black1 right">
+                        <div className="col-md-12 blackk">
+                            <button className="btn btn-light btn1">
+                                COMING SOON
+                            </button>
+                        </div>
+                        <div>
+                            <p className="remote mt-3">
+                                Sell it
+                        </p>
+                            <p className="branding">
+                                Branding, App
+                         </p>
+                        </div>
+                    </div>
+
+
+                </div>
+                
+            </div>
+        </>
+
+    )
+}
+
+
+export default Body
+
+{/* <div className="container">
+                <div className="row justify-content-start mt-5 first-img ">
+                    <div className="col-4 p-2">
+                        <img className="img1" src="./images/rz logo.png" alt="#" /><br></br>
+                        <img className="img2 ml-2" src="./images/puttingAfrica.png" alt="#" />
+                    </div>
+                </div>
+
+
+                <div className="row justify-content-center ml-4 rz">
+                    <div className="col text-left">
+                        <h1 className="rz1">
+                            Here at ReachZone,<br></br>
+                            we design interactions and convey ideas
+                            </h1>
+                    </div>
+
+                </div>
+
+                <div className="row justify-content-center ml-4">
+                    <div className="col text-left ">
+                        <p className="rz2 mt-5">
+                            We design, build readymade websites, mobile applications, and products
+                            that are simple, beautiful and helps businesses grow.
+                        </p>
+                    </div>
+                </div>
+
+
 
                 <div className="row justify-content-center">
                     <div className="col-12 text-center">
+                        
                         <img className="csb text-center" src="./images/csg.png" alt="#" />
                         <img className="RectangleBig text-center" src="./images/RectangleBig.png" alt="#" />
 
@@ -43,7 +138,7 @@ const Body = () => {
 
                 </div>
                 <div className="row justify-content-start ml-2">
-                    <div className="col">
+                    <div className="col ">
                         <p className="remote">
                             RemoteAfrica
                         </p>
@@ -55,7 +150,7 @@ const Body = () => {
                 </div>
 
 
-                <div className="row justify-content-between mb-2 ">
+                <div className="row justify-content-between mb-2  ">
                     <div className="col-12 col-md-5 mb-2">
                         <img className=" csb1  text-center" src="./images/csg.png" alt="#" />
                         <img className="RectangleSmall" src="./images/RectangleSmall.png" alt="#" />
@@ -96,11 +191,4 @@ const Body = () => {
 
                 </div>
 
-            </div>
-        </>
-
-    )
-}
-
-
-export default Body
+            </div> */}
